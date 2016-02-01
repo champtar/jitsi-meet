be94c40b8d35b15247795d7fad8823d38208cf13
Etienne CHAMPETIER <champetier.etienne@gmail.com>
2016-02-01 15:36:45 +0100

    !!!! config fiducial
    Signed-off-by: Etienne CHAMPETIER <champetier.etienne@gmail.com>

--- config.js
+++ config.js
@@ -2,33 +2,33 @@
 var config = {
 //    configLocation: './config.json', // see ./modules/HttpConfigFetch.js
     hosts: {
-        domain: 'jitsi-meet.example.com',
-        //anonymousdomain: 'guest.example.com',
-        //authdomain: 'jitsi-meet.example.com',  // defaults to <domain>
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
+        //domain: 'fiducial.net',
+        anonymousdomain: 'anon.fiducial.net',
+        authdomain: 'fiducial.net',
+        muc: 'muc.jitsi.fiducial.net',
+        //bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
         //jirecon: 'jirecon.jitsi-meet.example.com',
         //call_control: 'callcontrol.jitsi-meet.example.com',
-        //focus: 'focus.jitsi-meet.example.com', // defaults to 'focus.jitsi-meet.example.com'
+        focus: 'jicofo.jitsi.fiducial.net',
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
-    clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
+    bosh: '/http-bind',
+    clientNode: 'Fiducom', // The name of client node advertised in XEP-0115 'c' stanza
     //focusUserJid: 'focus@auth.jitsi-meet.example.com', // The real JID of focus participant - can be overridden here
     //defaultSipNumber: '', // Default SIP number
 
     // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
     desktopSharingChromeMethod: 'ext',
     // The ID of the jidesha extension for Chrome.
-    desktopSharingChromeExtId: 'diibjkoicjeejcmhdnailmkgecihlobk',
+    desktopSharingChromeExtId: 'abkpepjnljieihilablelpopponldomi',
     // The media sources to use when using screen sharing with the Chrome
     // extension.
     desktopSharingChromeSources: ['screen', 'window'],
     // Required version of Chrome extension
-    desktopSharingChromeMinExtVersion: '0.1',
+    desktopSharingChromeMinExtVersion: '0.0.1',
 
     // The ID of the jidesha extension for Firefox. If null, we assume that no
     // extension is required.
@@ -49,7 +49,7 @@ var config = {
     // Disables ICE/TCP by filtering out local and remote TCP candidates in signalling.
     webrtcIceTcpDisable: false,
 
-    openSctp: true, // Toggle to enable/disable SCTP channels
+    openSctp: false, // Toggle to enable/disable SCTP channels
     disableStats: false,
     disableAudioLevels: false,
     channelLastN: -1, // The default value of the channel attribute last-n.
@@ -59,15 +59,15 @@ var config = {
     enableWelcomePage: true,
     disableSimulcast: false,
     logStats: false, // Enable logging of PeerConnection stats via the focus
-//    requireDisplayName: true, // Forces the participants that doesn't have display name to enter it when they enter the room.
+    requireDisplayName: true,
 //    startAudioMuted: 10, // every participant after the Nth will start audio muted
 //    startVideoMuted: 10, // every participant after the Nth will start video muted
-//    defaultLanguage: "en",
+    defaultLanguage: "fr",
 // To enable sending statistics to callstats.io you should provide Applicaiton ID and Secret.
 //    callStatsID: "", // Application ID for callstats.io API
 //    callStatsSecret: "", // Secret for callstats.io API
     /*noticeMessage: 'Service update is scheduled for 16th March 2015. ' +
     'During that time service will not be available. ' +
     'Apologise for inconvenience.',*/
-    disableThirdPartyRequests: false,
+    disableThirdPartyRequests: true,
 };
